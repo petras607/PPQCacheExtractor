1fead7ba30e96156b444329752d45b305a7d4fe7
Courtroom <petras607@gmail.com>
2019-03-03 00:04:45 +0100

    Using knowledge of header information to modify extraction and start working on repacking

--- PPQCacheExtractor/PPQCacheExtractor.cpp
+++ PPQCacheExtractor/PPQCacheExtractor.cpp
@@ -45,11 +45,15 @@ std::wstring s2ws(std::string s)
 
 int extract(char cacheName[])
 {
-	int fileCount;
 	char* cacheFileBytes;
-	std::vector<int> fileSize;
+
+	unsigned int fileCount;
+	std::vector<unsigned int> fileSize;
+	std::vector<unsigned int> fileNameStart;
+	std::vector<unsigned int> fileContentStart;
 	std::vector<std::string> fileName;
 	std::vector<unsigned char> curFileBytes;
+
 	std::string baseDir(cacheName);
 
 	baseDir += "_out/";
@@ -68,18 +72,32 @@ int extract(char cacheName[])
 	if (cacheFileBytes == NULL)
 		return 1;
 
-	// Get first 2 bytes and store them in fileCount
-	fileCount = (int)((unsigned char)cacheFileBytes[0] | (unsigned char)cacheFileBytes[1] << 8);
+	// Get first 4 bytes and store them in fileCount
+	fileCount = (unsigned int)((unsigned char)cacheFileBytes[0] | (unsigned char)cacheFileBytes[1] << 8 | (unsigned char)cacheFileBytes[1] << 16 | (unsigned char)cacheFileBytes[1] << 24);
 
 	// Starting from byte 4 fill the fileSize vector with file sizes read as 4 bytes fileCount times.
+	int pos = 4;
 	for (int i = 0; i < fileCount; i++)
-		fileSize.push_back((int)((unsigned char)cacheFileBytes[4 + i * 4] | (unsigned char)cacheFileBytes[4 + i * 4 + 1] << 8
-			| (unsigned char)cacheFileBytes[4 + i * 4 + 2] << 16 | (unsigned char)cacheFileBytes[4 + i * 4 + 3] << 24));
+	{
+		fileSize.push_back((unsigned int)((unsigned char)cacheFileBytes[pos + i * 4] | (unsigned char)cacheFileBytes[pos + i * 4 + 1] << 8
+			| (unsigned char)cacheFileBytes[pos + i * 4 + 2] << 16 | (unsigned char)cacheFileBytes[pos + i * 4 + 3] << 24));
+	}
+	
+	// Read fileCount vaules and assign them to fileNameStart
+	pos += fileCount * 4;
+	for (int i = 0; i < fileCount; i++)
+	{
+		fileNameStart.push_back((unsigned int)((unsigned char)cacheFileBytes[pos + i * 4] | (unsigned char)cacheFileBytes[pos + i * 4 + 1] << 8
+			| (unsigned char)cacheFileBytes[pos + i * 4 + 2] << 16 | (unsigned char)cacheFileBytes[pos + i * 4 + 3] << 24));
+	}
 
-
-	// There are 8 bytes for each file that I don't know the purpose for yet. We skip them since they're not needed for extraction
-	int pos = 4 + 4 * fileCount + 8 * fileCount;
-	// TODO: Find the purpose of these 8 bytes
+	// Read fileCount vaules and assign them to fileContentStart
+	pos += fileCount * 4;
+	for (int i = 0; i < fileCount; i++)
+	{
+		fileContentStart.push_back((unsigned int)((unsigned char)cacheFileBytes[pos + i * 4] | (unsigned char)cacheFileBytes[pos + i * 4 + 1] << 8
+			| (unsigned char)cacheFileBytes[pos + i * 4 + 2] << 16 | (unsigned char)cacheFileBytes[pos + i * 4 + 3] << 24));
+	}
 
 	std::string curFileName;
 	std::string path;
@@ -89,9 +107,8 @@ int extract(char cacheName[])
 		// Reset the file name string
 		curFileName.clear();
 
-		// Find the next non 00 byte
-		while (cacheFileBytes[pos] == 0)
-			pos++;
+		// Go to the start of file name
+		pos = fileNameStart[i];
 
 		// Copy every char into the string until you encounter 00
 		while (cacheFileBytes[pos] != 0)
@@ -125,9 +142,8 @@ int extract(char cacheName[])
 	{
 		curFileBytes.clear();
 
-		// Find the next non 00 byte
-		while (cacheFileBytes[pos] == 0)
-			pos++;
+		// Go to the start of file content
+		pos = fileContentStart[i];
 
 		// Copy fileSize bytes into curFileBytes.
 		for (int j = 0; j < fileSize[i]; j++)
@@ -150,11 +166,51 @@ int extract(char cacheName[])
 	return 0;
 }
 
+int repack(char cacheName[], char directory[])
+{
+	std::string cache_old(cacheName);
+	std::string new_files(directory);
+	char* cacheFileBytes;
+
+	//int fileCount;
+
+	/*TODO:
+	1) Open cacheName
+	2) Get old attributes of cacheName: fileCount, fileSize, fileNameStart, fileContentStart, secretBytesStart, secretBytesContent
+	3) If the file count in directory is different than in the cache, return error
+	4) Get the list of files from directory, remove directory path from names
+	5) If a file path doesn't fit the one in the cache, return error
+	6) Modify parameters as such:
+		a) if file's size is different: offset every fileContentStart after the file and secretBytesStart with the difference
+		b) if file's name is different: offset every fileNameStart after the file, every fileContentStart and 
+	*/
+
+
+	// Copy bytes from cache file into cacheFileBytes
+	//cacheFileBytes = FileIntoByteVector(cacheName);
+
+	
+
+	std::cout << "You chose to repack!\n";
+
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		std::cout << "Give at least one file to extract from.\n";
+		std::cout << "Usage:\n";
+		std::cout << "1) Extraction: PPQCacheExtractor.exe file_path_1 file_path_2 ...\n";
+		std::cout << "2) Repacking: PPQCacheExtractor.exe -r cache_file directory_with_new_files\n";
+		return 0;
+	}
+
+	// Check if the parameter for repacking is passed
+	std::string arg1(argv[1]);
+	if (arg1 == "-r")
+	{
+		repack(argv[2], argv[3]);
 		return 0;
 	}
 
